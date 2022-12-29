ea2df6ce890a6293724808268cc8cb58078dc016
Najima Sahloumi <nsahloum@gmail.com>
2022-12-29 17:38:31 +0100

    EventEmitter

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class HeaderComponent {
   title = 'TaskManager';
+
+  toggleAddTask() {
+    console.log('toggle')
+  }
 }
